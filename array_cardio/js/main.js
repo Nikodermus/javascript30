e83289cada2fd64094ab2212f8f6dcd77a1100b8
Nicolás M. Pardo <nicolasm@nikodermus.media>
2017-11-21 21:36:52 -0500

    Array Cardio 2

--- array_cardio/js/main.js
+++ array_cardio/js/main.js
@@ -40,8 +40,11 @@ const mangaka = [{
 
 //Born in 60's
 const mangaka_60 = mangaka.filter(elem => elem.year > 1959 && elem.year < 1970);
+//List names
 const mangaka_name = mangaka.map(elem => `${elem.first} ${elem.last}`);
+
 const mangaka_order = mangaka.sort((a, b) => a.year > b.year ? 1 : -1);
+
 const mangaka_alive = mangaka.reduce((total, inventor) => total + (inventor.passed - inventor.year), 0);
 const mangaka_older = mangaka.sort((a, b) => {
 	first = a.passed - a.year;
@@ -49,7 +52,6 @@ const mangaka_older = mangaka.sort((a, b) => {
 	return first < last ? 1 : -1;
 });
 
-console.log(mangaka_60, mangaka_name, mangaka_order, mangaka_alive, mangaka_older);
 
 //Get Items from Page
 const links = Array.from(document.querySelectorAll('.mw-category a'));
@@ -66,7 +68,6 @@ const names_sorted = names.sort((a, b) => {
 	return aLast > bLast ? 1 : -1;
 }).map(elem => elem.replace(',', ''));
 
-console.log(names_sorted);
 
 //How many of each?
 const vehicles = ['car', 'truck', 'car', 'truck', 'car', 'truck', 'car', 'bike', 'truck', 'car', 'bike', 'walk'];
@@ -78,7 +79,53 @@ const vehicles_data = vehicles.reduce((loader, item) => {
 	return loader;
 }, {});
 
-console.log(vehicles_data);
+/* ARRAY CARDIO DAY 2 */
 
+const people = [{
+		name: 'Carl',
+		year: 1964
+	},
+	{
+		name: 'Marx',
+		year: 1970
+	},
+	{
+		name: 'Lennin',
+		year: 1994
+	},
+	{
+		name: 'Stanlin',
+		year: 1999
+	},
+	{
+		name: '',
+		year: 2005
+	}
+];
 
-window.onload = () => {};
+const comments = [{
+		text: 'I want some pizza for breakfast',
+		id: 13248
+	},
+	{
+		text: 'I\'m so sleepy',
+		id: 13989
+	},
+	{
+		text: 'Let\'s play some DOOM',
+		id: 1
+	},
+	{
+		text: 'I should have got to slept more time',
+		id: 18393
+	}
+];
+
+const is_there_adult = people.some(person => new Date().getFullYear() - person.year >= 19);
+
+const are_all_adults = people.every(person => new Date().getFullYear() - person.year >= 19);
+
+const comment = comments.find(comment => comment.id === 13989);
+const comment_index = comments.findIndex(comment => comment.id === 1);
+
+console.log(comments.splice(comment_index, comments.length));
