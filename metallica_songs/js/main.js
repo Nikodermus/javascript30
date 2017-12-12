7dc91d8d200dd630a2e65443addeed1f171f4ce4
Nicolás M. Pardo <nicolasm@nikodermus.media>
2017-12-12 10:12:58 -0500

    Metallica Songs

---
+++ metallica_songs/js/main.js
@@ -0,0 +1,32 @@
+/*jshint esversion: 6 */
+const songs = [
+	"Master of Puppets",
+	"Fade to Black",
+	"One",
+	"For Whom the Bell Tolls",
+	"Enter Sandman",
+	"The Unforgiven",
+	"The Four Horsemen",
+	"The Day that Never Comes",
+	"Of Wolf and Men"
+];
+
+let ul;
+
+const articles = ["the", "of", "a"];
+
+function replaceName(params) {
+	return params.replace(/^(a|the|of)\b/i, '').trim();
+}
+
+window.onload = () => {
+	ul = document.querySelector('ul');
+
+	const markup = songs
+		.sort((a, b) => replaceName(a) > replaceName(b) ? 1 : -1)
+		.map(song => `<li>${song}</li>`)
+		.join('');
+
+	ul.innerHTML = markup;
+
+};
